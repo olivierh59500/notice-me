18f1ef8d4866e81b1f0ae989e1f5013a988b55db
719Ben <ben.williams@colorado.edu>
2016-03-28 20:36:08 -0600

    Added README, merged example.py in notice_me.py

---
+++ README.md
@@ -0,0 +1,20 @@
+# attention-whore
+A proof of concept to show the way project collaborators notifications are handled in Github is flawed.
+
+## What?
+`attention-whore` is a quick way to search for the most followed people on Github and then quickly add and remove them as a collaborator your project.
+This will alert their followers that they have been added to your project, dramatically increasing the reach it has to Github users.
+
+## How?
+[Download](https://github.com/719Ben/attention-whore/archive/master.zip) or clone `attention-whore` to your computer.
+```bash
+git clone git@github.com:719Ben/attention-whore.git
+```
+Make sure that you have both [python3](https://www.python.org/downloads/) and `requests` installed.
+```bash
+pip install requests
+```
+Run attention-whore, a great, useful example file is included when downloading or cloning.
+```bash
+python3 notice_me.py
+```
--- notice_me.py
+++ notice_me.py
@@ -45,5 +45,31 @@ class AttentionWhore(object):
         remove_request = requests.delete(
             request_string, auth=(self.gh_username, self.gh_access))
 
+
 if __name__ == "__main__":
-    print("notice-me.py is a library. try example.py")
+
+    username = input("github username: ")
+    password = input("github password (or access code): ")
+    project = input("github project: ")
+    star_count = input(
+        "how many stars should your \"collaborators\" have? (1000) ")
+
+    if star_count == "":
+        star_count = 1000
+    else:
+        try:
+            star_count = int(star_count)
+        except:
+            star_count = 1000
+
+    kardashian = AttentionWhore(username, password)
+
+    print("getting the user list")
+
+    user_list = kardashian.get_users(star_count)
+
+    print("adding \"collaborators\"")
+
+    for user in user_list:
+        print(user)
+        kardashian.touch_collaborator(project, user)
